4bb1e0f5d9d9a1d84cca620f8bdbaab1819523a9
Freddie <freddie@muzmatch.com>
2020-11-28 18:37:57 +0000

    Add input validation

--- generatePrimeTable.js
+++ generatePrimeTable.js
@@ -2,6 +2,12 @@ const generatePrimeTable = (input) => {
     if (typeof input !== 'number') {
         return false;
     }
+
+    if (input < 1) {
+        return false;
+    }
+
+    return true;
 }
 
 module.exports = generatePrimeTable;
--- generatePrimeTable.test.js
+++ generatePrimeTable.test.js
@@ -1,5 +1,19 @@
 const generatePrimeTable = require('./generatePrimeTable');
 
-it('Should give negative result for string', () => {
-    expect(generatePrimeTable('10')).toBe(false);
+describe('Validate input', () => {
+    describe('Check input type', () => {
+        it('Should give negative result where input is a string', () => {
+            expect(generatePrimeTable('10')).toBe(false);
+        })
+        
+        it('Should give positive result where input is an integer', () => {
+            expect(generatePrimeTable(10)).toBe(true);
+        })
+    })
+
+    describe('Check input is at least one', () => {
+        it('Should give negative result where input is less than 1', () => {
+            expect(generatePrimeTable(-4)).toBe(false);
+        })
+    })
 })
