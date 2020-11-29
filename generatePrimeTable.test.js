27d9fade00fa3363603f594bb61f7bc6755be91e
Freddie <freddie@muzmatch.com>
2020-11-29 16:46:14 +0000

    Remove exported test

--- generatePrimeTable.test.js
+++ generatePrimeTable.test.js
@@ -17,15 +17,3 @@ describe('Validate input', () => {
         })
     })
 })
-
-describe('Test multiplication tables', () => {
-    describe('Should give a 2x2 table when N is 1', () => {
-        it('Should return a string with the correct format', () => {
-            const result = generatePrimeTable(1);
-            const regex = /^(((\|(\|\d+){1}\|)\n){1}(((\|\d+){2}\|)(\n)){0}((\|\d+){2}\|){1})$/;
-            const isCorrectFormat = !!result.match(regex)
-
-            expect(isCorrectFormat).toEqual(true);
-        })
-    })
-})
