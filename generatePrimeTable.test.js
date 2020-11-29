b865f2e8ae05d55f80c1c413ac3bc779b4182c6c
Freddie <freddie@muzmatch.com>
2020-11-29 15:23:14 +0000

    Remove duplicate tests

--- generatePrimeTable.test.js
+++ generatePrimeTable.test.js
@@ -20,34 +20,6 @@ describe('Validate input', () => {
 
 describe('Test multiplication tables', () => {
     describe('Should give a 2x2 table when N is 1', () => {
-        it('Should return a string with one newline character, indicating two rows', () => {
-            const result = generatePrimeTable(1);
-            const matches = result.match(/\n/g) || [];
-            const count = matches.length;
-
-            expect(count).toEqual(1);
-        })
-
-        it('Should return a string with three bars on the first row', () => {
-            const result = generatePrimeTable(1);
-            const rows = result.split('\n');
-            const firstRow = rows[0];
-            const matches = firstRow.match(/\|/g) || [];
-            const count = matches.length;
-
-            expect(count).toEqual(3);
-        })
-
-        it('Should return a string with three bars on the second row', () => {
-            const result = generatePrimeTable(1);
-            const rows = result.split('\n');
-            const secondRow = rows[1];
-            const matches = secondRow.match(/\|/g) || [];
-            const count = matches.length;
-
-            expect(count).toEqual(3);
-        })
-
         it('Should return a string with the correct format', () => {
             const result = generatePrimeTable(1);
             const regex = /^(((\|(\|\d+){1}\|)\n){1}(((\|\d+){2}\|)(\n)){0}((\|\d+){2}\|){1})$/;
