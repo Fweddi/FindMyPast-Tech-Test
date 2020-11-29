e67ec53e341652c6a361f68480415b5004d783a3
Freddie <freddie@muzmatch.com>
2020-11-29 15:20:45 +0000

    Improve formatting

--- generatePrimeTable.js
+++ generatePrimeTable.js
@@ -1,13 +1,40 @@
-const generatePrimeTable = (input) => {
-    if (typeof input !== 'number') {
+const generatePrimeTable = (n) => {
+    if (typeof n !== 'number') {
         return false;
     }
 
-    if (input < 1) {
+    if (n < 1) {
         return false;
     }
 
-    return '||2|\n|2|4|'
+    let primes = generateNPrimes(n);
+
+    let multiples = multiplyPrimes(primes);
+
+    return formatRows(primes, multiples);
+}
+
+const generateNPrimes = (n) => {
+    return [2];
+}
+
+const multiplyPrimes = (primes) => {
+    return {
+        2: {
+            2: 4
+        }
+    }
+}
+
+const formatRows = (primes, multiples) => {
+    let firstRow = '||' + primes.join('|') + '|';
+
+    let remainingRows = primes.map(prime => {
+        let primeMultiples = Object.values(multiples[prime]);
+        return '|' + prime + '|' + primeMultiples.join('|') + '|';
+    });
+
+    return firstRow + '\n' + remainingRows.join('\n');
 }
 
 module.exports = generatePrimeTable;
--- generatePrimeTable.test.js
+++ generatePrimeTable.test.js
@@ -47,5 +47,13 @@ describe('Test multiplication tables', () => {
 
             expect(count).toEqual(3);
         })
+
+        it('Should return a string with the correct format', () => {
+            const result = generatePrimeTable(1);
+            const regex = /^(((\|(\|\d+){1}\|)\n){1}(((\|\d+){2}\|)(\n)){0}((\|\d+){2}\|){1})$/;
+            const isCorrectFormat = !!result.match(regex)
+
+            expect(isCorrectFormat).toEqual(true);
+        })
     })
 })
