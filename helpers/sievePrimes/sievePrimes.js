d0e885a40ce41142d9a84a19f211ddd37b27ae81
Freddie <freddie@muzmatch.com>
2020-11-30 20:53:31 +0000

    Reduce complexity of sieve

--- helpers/sievePrimes/sievePrimes.js
+++ helpers/sievePrimes/sievePrimes.js
@@ -1,39 +1,46 @@
 const sievePrimes = (limit, primeCount) => {
-    if(limit <= 1) {
+    if (limit <= 1) {
         return [];
     }
-    
-    // Create a results list, filled with 2, 3 and 5
-    let results = [2, 3, 5];
+
+    let results = [];
+
+    let limitSqrt = Math.sqrt(limit);
+    let sieve = [];
+    let n;
+
+    sieve[2] = true;
+    sieve[3] = true;
 
     // Create a sieve list with an entry for each positive integer 
-    // All entries of this list should initially be marked nonprime (i.e. false)
-    let sieve = [];
-    for (let i = 1; i <= limit; i++) {
-        sieve.push(false);
-    }
+    // All entries of this list should initially be marked nonprime (i.e. false), except for 2 and 3
+    for (let x = 1; x <= limitSqrt; x++) {
+        let xx = x * x;
+        for (let y = 1; y <= limitSqrt; y++) {
+            let yy = y * y;
+            if (xx + yy >= limit) {
+                break;
+            }
+            // Flip n according to different quadratic criteria
+            n = (4 * xx) + (yy);
+            if (n <= limit && (n % 12 == 1 || n % 12 == 5)) {
+                sieve[n] = !sieve[n];
+            }
+            
+            n = (3 * xx) + (yy);
+            if (n <= limit && (n % 12 == 7)) {
+                sieve[n] = !sieve[n];
+            }
 
-    // Flip n according to different quadratic criteria
-    sieve.forEach((_isPrime, n) => {
-        let r = n % 60;
-
-        for (let x = 1; x * x <= limit; x++) {
-            for (let y = 1; y * y <= limit; y++) {
-                if (x * x + y * y >= limit) {
-                    break;
-                }
-
-                if (([1, 13, 17, 29, 37, 41, 49, 53].includes(r) && n === ((4 * x * x) + (y * y)))
-                    || ([7, 19, 31, 43].includes(r) && n === ((3 * x * x) + (y * y)))
-                    || ([11, 23, 47, 59].includes(r) && x > y && n === ((3 * x * x) - (y * y)))) {
-                    sieve[n] = !sieve[n];
-                }
+            n = (3 * xx) - (yy);
+            if (x > y && n <= limit && (n % 12 == 11)) {
+                sieve[n] = !sieve[n];
             }
         }
-    })
+    }
 
     // Mark multiples of prime squares as false (not needed for multiples of 2, 3 and 5)
-    for (n = 5; n * n <= limit; n++) {
+    for (n = 5; n <= limitSqrt; n++) {
         if (sieve[n]) {
             x = n * n;
             for (i = x; i <= limit; i += x) {
@@ -41,7 +48,6 @@ const sievePrimes = (limit, primeCount) => {
             }
         }
     }
-
     // If sieve value is true, push index to results
     sieve.forEach((isPrime, n) => {
         if (isPrime) {
